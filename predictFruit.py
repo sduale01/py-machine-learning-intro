ce1ff5dba99e51ba05d93d89d5f77532f479969f
Sharmarke Duale <sduale01@gmail.com>
2019-04-04 23:49:23 -0500

    predict type of flower within a set of data

--- predictFruit.py
+++ predictFruit.py
@@ -1,9 +1,9 @@
 # import sys
 from sklearn import tree
-feature = [[140, 1], [130, 1], [150, 0], [170, 0]]
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 labels = [0,0,1,1]
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(feature, labels) 
+clf = clf.fit(features, labels) 
 
 
 print(clf.predict([[150, 0]]))
