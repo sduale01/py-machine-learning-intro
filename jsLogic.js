f63b086b92c497e5134081c9e1a8d88982ca56fc
Sharmarke Duale <sduale01@gmail.com>
2019-04-07 15:42:56 -0500

    sort data by given parameter

--- jsLogic.js
+++ jsLogic.js
@@ -6,5 +6,38 @@ for (let i = 1; i < 17; i++) {
     if(i % 5 === 0){output += 'Buzz'}
 
     if(output == ''){output = i}
-    console.log(output);
+    // console.log(output);
 }
+let sensors =[
+    {
+        timestamp: "2019-03-07T19:14:30Z",
+        sensor: "Sensor 1",
+        people: 0
+    },
+    {
+        timestamp: "2019-03-07T19:19:35Z",
+        sensor: "Sensor 2",
+        people: 0
+    },
+    {
+        timestamp: "2019-03-07T19:24:43Z",
+        sensor: "Sensor 1",
+        people: 0
+    },
+    {
+        timestamp: "2019-03-07T19:29:49Z",
+        sensor: "Sensor 2",
+        people: 0
+    }
+];
+  sensors.sort((a,b) => {
+
+    a = a.sensor;
+    b = b.sensor;
+    return a<b ? -1 : a>b ? 1 : 0;
+})
+
+console.log('sorted sensors', sensors);
+
+  
+
