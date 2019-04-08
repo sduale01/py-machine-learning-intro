0444b4a3d04df99b59f2cdda9da0192b77d4af72
Sharmarke Duale <sduale01@gmail.com>
2019-04-07 23:24:34 -0500

    built classifier

--- predictFruit.py
+++ predictFruit.py
@@ -11,9 +11,18 @@ class ScrappyKNN:
     def predict(self, x_test):
         predictions = []
         for row in x_test:
-            label = random.choice(self.y_train)
+            label = self.closest(row)
             predictions.append(label)
         return predictions
+    def closest(self, row):
+        best_dist = euc(row, self.x_train[0])
+        best_index = 0
+        for i in range(1, len(self.x_train)):
+            dist = euc(row, self.x_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+            return self.y_train[best_index]
 
 from sklearn import datasets 
 
