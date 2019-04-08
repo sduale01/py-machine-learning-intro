65b2b4f0716f7a7fe6a4549ab5eef5cef354e75b
Sharmarke Duale <sduale01@gmail.com>
2019-04-07 23:09:25 -0500

    create classifier with 33 perc accuracy

--- predictFruit.py
+++ predictFruit.py
@@ -1,4 +1,15 @@
+import random
+
 class ScrappyKNN:
+    def fit(self, x_train, y_train):
+        self.x_train = x_train
+        self.y_train = y_train
+    def predict(self, x_test):
+        predictions = []
+        for row in x_test:
+            label = random.choice(self.y_train)
+            predictions.append(label)
+        return predictions
 
 from sklearn import datasets
 
