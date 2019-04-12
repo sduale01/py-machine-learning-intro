538e2068f323edf29c139a62cc6042b135c68a28
Sharmarke Duale <sduale01@gmail.com>
2019-04-12 00:53:59 -0500

    display how the comp sees clothing

--- classifyClothes.py
+++ classifyClothes.py
@@ -14,10 +14,19 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+# uncomment the following line to download fashion mnist dataset
 # print(fashion_mnist)
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-print(train_images.shape)
-print(len(train_labels))
+# print("images, each image frame is 28x28:", train_images.shape)
+# print("labels in training set:", len(train_labels))
+# print("images, each image frame is 28x28:", test_images.shape)
+# print("labels in testing set:", len(test_labels))
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+print(plt.show())
