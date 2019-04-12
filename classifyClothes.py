eebae753042ed1ce9addb7289e87a988e2a35a2b
Sharmarke Duale <sduale01@gmail.com>
2019-04-12 00:35:16 -0500

    download fashon mnsit dataset

--- classifyClothes.py
+++ classifyClothes.py
@@ -1,1 +1,23 @@
 from __future__ import absolute_import, division, print_function
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("tensor flow version:", tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# print(fashion_mnist)
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+print(len(train_labels))
